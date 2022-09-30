3fe216c947c4624935331c035baf71441f8ef607
psyhotel <sative2010@yandex.ru>
2022-09-30 17:49:38 +0500

    Calculator fix2

--- src/Calc.java
+++ src/Calc.java
@@ -7,28 +7,27 @@ class Calc {
         return RomainDigits.valueOf(numArabian);
     }
 
-    static int romanToNumber(String roman) {
-        return RomainDigits.indexOf(roman);
-    }
-
     public static void calc(Scanner input) {
         int num1 = 0, num2 = 0;
         String operand = "";
         String[] str;
-        boolean isRoman;
+        boolean isRoman = false;
         if (input.hasNextInt()) {
             str = input.nextLine().split(" ");
+            if (str.length < 3) {
+                throw new RuntimeException("Строка не является математической операцией");
+            }
+            if (str.length > 3) {
+                throw new RuntimeException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            }
             num1 = Integer.parseInt(str[0]);
             operand = str[1];
-            if (str.length > 3) {
-                throw new RuntimeException("Больше двух операндов");
-            }
             try {
                 num2 = Integer.parseInt(str[2]);
+
             } catch (Exception e) {
-                throw new CalculatorNumberFormatException("Ошибка: ");
+                throw new NumberFormatException("Ошибка: ");
             }
-            isRoman = false;
         } else {
             str = input.nextLine().split(" ");
 
@@ -37,16 +36,17 @@ class Calc {
                 operand = str[1];
                 num2 = romanToNumber(str[2]);
                 isRoman = true;
-
             } catch (Exception exception) {
-                throw new CalculatorNumberFormatException("Error: ");
-
-            }
-            if (str.length > 3) {
-                throw new RuntimeException("Больше двух операндов");
+                throw new NumberFormatException("Ошибка: ");
             }
 
         }
+
+        if (str.length > 3) {
+            throw new RuntimeException("Больше двух операндов");
+        }
+
+
         int results = 0;
         if (num1 > 10 || num2 > 10) {
             throw new RuntimeException("Число больше 10");
@@ -64,10 +64,10 @@ class Calc {
             throw new WrongSignException(operand + " ");
         }
 
-        if (results < 0) {
-            throw new RuntimeException("Число меньше 0");
+        if (isRoman && results < 0) {
+            throw new RuntimeException("в римской системе нет отрицательных чисел");
         }
-        if (isRoman && results == 0) {
+        if ((isRoman) && results == 0) {
             throw new RuntimeException("В римской системе счисления не может быть 0");
         }
 
@@ -76,6 +76,11 @@ class Calc {
         } else {
             System.out.println(results);
         }
+
+    }
+
+    static int romanToNumber(String roman) {
+        return RomainDigits.indexOf(roman);
     }
 
     public static void main(String[] args) throws Exception {
--- src/CalculatorNumberFormatException.java
+++
@@ -1,5 +0,0 @@
-public class CalculatorNumberFormatException extends java.lang.NumberFormatException {
-    public CalculatorNumberFormatException(String s) {
-        System.out.println(s + " Вы ввели неправильное число");
-    }
-}
