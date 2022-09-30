37deba649932a4a608b8c9cdbc1ec23d68892644
psyhotel <sative2010@yandex.ru>
2022-09-30 17:51:36 +0500

    Calculator fix3

---
+++ src/NumberFormatException.java
@@ -0,0 +1,5 @@
+public class NumberFormatException extends java.lang.NumberFormatException {
+    public NumberFormatException(String s) {
+        System.out.println(s + " используются одновременно разные системы счисления");
+    }
+}
